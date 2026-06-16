1ac64e6d0538538dbf04d6a4472542677ea5a3bf
Kirill Popkov <kirill.popkov.work@gmail.com>
2026-06-16 14:06:43 +0300

    upd: Обновление ядра Kafka Bus до 1.2 (#3)

--- tests/Fixtures/MessageMiddlewareFake.php
+++ tests/Fixtures/MessageMiddlewareFake.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace KafkaBus\Laravel\Tests\Fixtures;
 
 use KafkaBus\Core\Consumers\Pipelines\MessagePipelineHandler;
-use KafkaBus\Core\Consumers\Router\MessagePipelineMiddleware;
+use KafkaBus\Core\Consumers\Pipelines\MessagePipelineMiddleware;
 use KafkaBus\Core\Interfaces\Pipelines\PipelineInterface;
 
 final class MessageMiddlewareFake implements MessagePipelineMiddleware
 {
     /**
-     * @param PipelineInterface<MessagePipelineHandler> $pipeline
-     * @return PipelineInterface<MessagePipelineHandler>
+     * @param PipelineInterface<mixed, MessagePipelineHandler> $pipeline
+     * @return PipelineInterface<mixed, MessagePipelineHandler>
      */
     public function handle(PipelineInterface $pipeline): PipelineInterface
     {
